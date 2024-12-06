801ba1d2b14bd0dbd1c39893d74e24f7b91d5dfb
Hind Guessous <guessoushind@gmail.com>
2024-12-06 11:52:23 +0100

    Github pages config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: "/nextjs-github-pages",
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
